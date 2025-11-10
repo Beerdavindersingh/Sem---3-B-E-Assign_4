4390a429557d98c04c7ad59dc50d501e46bef820
Beerdavindersingh <beerdavindersingh@rrc.com>
2025-11-09 18:09:14 -0600

    updated loanRoutes file

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,18 +1,25 @@
 import express, { Router } from "express";
 import * as loanController from "../controllers/loanController";
+import authenticate from "../middleware/authenticate";
  
 const router: Router = express.Router();
  
 //Create a loan
-router.post("/", loanController.createLoan);
+router.post("/",
+    authenticate,
+    loanController.createLoan);
  
 //List all loans
-router.get("/", loanController.getAllLoans);
+router.get("/",
+    authenticate,
+    loanController.getAllLoans);
  
 //Review a specific loan
-router.post("/:id/review", loanController.reviewLoan);
+router.post("/:id/review",
+    authenticate,
+    loanController.reviewLoan);
  
 //Approve a specfic loan
-router.put("/:id/approve", loanController.approveLoan);
+router.put("/:id/approve",authenticate, loanController.approveLoan);
  
 export default router;
