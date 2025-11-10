bc4df7a75ac45c971fb59224981871e07ed74f90
Beerdavindersingh <beerdavindersingh@rrc.com>
2025-11-09 19:03:11 -0600

    added admin and user routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
 // import the express application and type definition
 import express, { Express } from "express";
- 
+
+import adminRoutes from "../src/api/v1/routes/adminRoutes";
 import loanRoutes from "./api/v1/routes/loanRoutes";
-import adminRoutes from "./api/v1/routes/adminRoutes";
+import userRoutes from "./api/v1/routes/userRoutes";
+import errorHandler from "./api/v1/middleware/errorHandler";
 import {
     accessLogger,
     errorLogger,
     consoleLogger,
 } from "../src/api/v1/middleware/logger";
- 
+
 // initialize the express application
 const app: Express = express();
- 
+
 // Interface for health check response
 // An interface in TypeScript defines the structure or "shape" of an object.
 interface HealthCheckResponse {
@@ -21,24 +23,25 @@ interface HealthCheckResponse {
     version: string;
 }
 // Middleware START
- 
+
 app.use(accessLogger);
 app.use(errorLogger);
 app.use(consoleLogger);
 
+// Ensures incoming body is correctly parsed to JSON, otherwise req.body would be undefined
 app.use(express.json());
- 
+
 // Middleware END
- 
+
 // respond to GET request at endpoint "/" with message
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
- 
+
 /**
-* Health check endpoint that returns server status information
-* @returns JSON response with server health metrics
-*/
+ * Health check endpoint that returns server status information
+ * @returns JSON response with server health metrics
+ */
 app.get("/api/v1/health", (req, res) => {
     const healthData: HealthCheckResponse = {
         status: "OK",
@@ -46,18 +49,19 @@ app.get("/api/v1/health", (req, res) => {
         timestamp: new Date().toISOString(),
         version: "1.0.0",
     };
- 
+
     res.json(healthData);
 });
- 
+
 // Route Imports START
 // "/api/v1/items" will prefix all item routes
-app.use("/api/v1/users", loanRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/loans", loanRoutes);
+app.use("/api/v1/users", userRoutes);
 
- 
 // Route Imports END
- 
+
 // needs to be used last
 app.use(errorHandler);
- 
+
 export default app;
